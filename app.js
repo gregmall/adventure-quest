6ebb88f29998e67ee1b0be9b43720aba27354e05
Greg Mall <gregmall157@gmail.com>
2020-07-23 12:54:20 -0700

    added user data

---
+++ app.js
@@ -0,0 +1,15 @@
+import { makeUser } from './userUtils.js';
+console.log('apps for home page');
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+
+    const user = makeUser(formData);
+
+    const stringyUser= JSON.stringify(user);
+    localStorage.setItem('USER', stringyUser);
+
+});
